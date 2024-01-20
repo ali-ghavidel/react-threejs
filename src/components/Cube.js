3c673d761aaf47bf95e9274cb4a970ee7ccca475
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-01-20 14:23:12 +0330

    plane geometry with gradiant shader

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -2,9 +2,11 @@ import React, { useRef } from 'react';
 
 
 const vertexShader = `
+varying vec2 vUv;
 void main() {
+    vUv = uv;
+
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.y += sin(modelPosition.x * 4.0) * 0.2;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
@@ -12,8 +14,17 @@ void main() {
 }
 `;
 const fragmentShader = `
+varying vec2 vUv;
+
+vec3 colorA = vec3(0.912,0.191,0.652);
+vec3 colorB = vec3(1.000,0.777,0.052);
+
 void main() {
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+
+    vec2 normalizedPixel = gl_FragCoord.xy/600.0;
+    vec3 color = mix(colorA, colorB, normalizedPixel.x);
+
+    gl_FragColor = vec4(color , 1.0);
   }
 `;
 
@@ -21,9 +32,9 @@ const Cube = () => {
     
     const mesh = useRef()
     return (
-        <mesh ref={mesh} position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0]} scale={1.5} >
+        <mesh ref={mesh} position={[0,0,0]} scale={1.0} >
             <planeGeometry args={[1, 1, 32, 32]} />
-            <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} wireframe />
+            <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} />
         </mesh>
     );
 }
