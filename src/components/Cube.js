acd041e652006bb47ddf0af946b02d2ada72742e
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-01-20 14:42:02 +0330

    animated by time flag gradiant

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,12 +1,14 @@
-import React, { useRef } from 'react';
-
+import React, { useMemo, useRef } from 'react';
+import { useFrame } from '@react-three/fiber'
 
 const vertexShader = `
+uniform float u_time;
 varying vec2 vUv;
 void main() {
     vUv = uv;
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.y += sin(modelPosition.x * 4.0 + u_time * 2.0) * 0.2;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
@@ -14,15 +16,16 @@ void main() {
 }
 `;
 const fragmentShader = `
+
 varying vec2 vUv;
 
-vec3 colorA = vec3(0.912,0.191,0.652);
-vec3 colorB = vec3(1.000,0.777,0.052);
+vec3 colorA = vec3(1.0,1.0,0.052);
+vec3 colorB = vec3(0.400,0.0,1.0);
 
 void main() {
 
-    vec2 normalizedPixel = gl_FragCoord.xy/600.0;
-    vec3 color = mix(colorA, colorB, normalizedPixel.x);
+    vec2 normalizedPixel = gl_FragCoord.xy/400.0;
+    vec3 color = mix(colorA, colorB, normalizedPixel.y);
 
     gl_FragColor = vec4(color , 1.0);
   }
@@ -31,10 +34,25 @@ void main() {
 const Cube = () => {
     
     const mesh = useRef()
+    const uniform = useMemo(()=>{
+        return (
+            {
+                u_time: {
+                    value: 0.0
+                }
+            }
+        )
+    },[]);
+
+    useFrame((state)=>{
+        const { clock } = state;
+        mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    })
+
     return (
-        <mesh ref={mesh} position={[0,0,0]} scale={1.0} >
+        <mesh ref={mesh} position={[0, 0, 0]}  rotation={[-Math.PI / 2, 0, 0]} scale={1.5} >
             <planeGeometry args={[1, 1, 32, 32]} />
-            <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} />
+            <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={uniform} wireframe />
         </mesh>
     );
 }
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -4,7 +4,7 @@ import { OrbitControls } from '@react-three/drei'
 import Cube from './Cube';
 const Scene = () => {
     return (
-        <Canvas className='canvas' camera={{position: [0.0,0.0,1.0], fov: 75}}>
+        <Canvas className='canvas' camera={{position: [1.0,1.0,1.0], fov: 75}}>
             <Cube />
             <axesHelper />
             <OrbitControls />
